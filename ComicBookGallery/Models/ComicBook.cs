e59df3971a1771dc5be4479c6b97bd577683f180
ihagerman <ian.hagerman@infoworks-tn.com>
2017-03-07 12:04:25 -0600

    updated comic book detail view to be strongly typed

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -10,7 +10,7 @@ namespace ComicBookGallery.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string DescrtiptionHtml { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
@@ -29,6 +29,6 @@ namespace ComicBookGallery.Models
                     .ToLower() + "-" + IssueNumber + ".jpg";
             }
 
-
+        }
     }
 }
